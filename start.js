30927ce4d138429d3c5a7983d6a5a29df06192da
Mxx1029 <lia.pim@icloud.com>
2021-03-30 13:26:25 +0200

    Initial commit

---
+++ start.js
@@ -0,0 +1,63 @@
+const snackServings = 3;
+const guests = 2;
+
+console.log(snackServings, guests)
+
+let total = snackServings + guests;
+
+console.log(total);
+
+total = snackServings - guests;
+
+console.log(total);
+
+total = guests - snackServings;
+
+console.log(total);
+
+total = snackServings * guests;
+
+console.log(total);
+
+total = snackServings / guests;
+
+console.log(total);
+
+const drinks = 10;
+
+total = snackServings * guests;
+
+const resultOne = total / drinks;
+
+console.log(resultOne);
+
+let monkeys = 15;
+
+let bananas = 9;
+
+const howManyBananasPerMOnkey = monkeys / bananas;
+
+console.log(howManyBananasPerMOnkey);
+
+const monkeyFights = 20;
+
+const resultTwo = (monkeys + bananas) * monkeyFights;
+
+console.log(resultTwo);
+
+monkeys++;
+
+console.log(monkeys);
+
+bananas--;
+
+console.log(bananas);
+
+const happiness = bananas - monkeys;
+
+const monkeyWorld = happiness + monkeyFights;
+
+console.log(monkeyWorld);
+
+console.log(resultOne%resultTwo);
+
